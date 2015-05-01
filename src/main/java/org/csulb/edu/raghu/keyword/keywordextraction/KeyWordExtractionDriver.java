f6c190561b74531a990921f19d8eff4b0c8c1615
Goutam Tadi <goutam.tadi1@gmail.com>
2015-04-30 23:00:12 -0700

    regex commit

--- src/main/java/org/csulb/edu/raghu/keyword/keywordextraction/KeyWordExtractionDriver.java
+++ src/main/java/org/csulb/edu/raghu/keyword/keywordextraction/KeyWordExtractionDriver.java
@@ -2,7 +2,10 @@ package org.csulb.edu.raghu.keyword.keywordextraction;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -11,7 +14,7 @@ import org.csulb.edu.regex.RegexFileInputFormat;
 
 public class KeyWordExtractionDriver extends Configured implements Tool {
 
-	public int run(String[] arg0) throws Exception {
+	public int run(String[] args) throws Exception {
 
 		Configuration configuration = this.getConf();
 		Job job = new Job(configuration, KeyWordExtractionConstants.JOBNAME);
@@ -20,6 +23,10 @@ public class KeyWordExtractionDriver extends Configured implements Tool {
 		job.setNumReduceTasks(KeyWordExtractionConstants.ZERO);
 		job.setInputFormatClass(RegexFileInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
 
 		return job.waitForCompletion(KeyWordExtractionConstants.TRUE) ? KeyWordExtractionConstants.ZERO
 				: KeyWordExtractionConstants.ONE;
---
+++ src/main/java/org/csulb/edu/raghu/keyword/util/KeyWordExtractionConstants.java
@@ -0,0 +1,9 @@
+package org.csulb.edu.raghu.keyword.util;
+
+public class KeyWordExtractionConstants {
+
+	public final static String JOBNAME = "KeyWordExtraction";
+	public final static boolean TRUE = true;
+	public final static int ZERO = 0;
+	public final static int ONE = 1;
+}
