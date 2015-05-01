4f90afd55a042dc6352a971c67281c5356b8b6c3
Goutam Tadi <goutam.tadi1@gmail.com>
2015-04-30 22:52:34 -0700

    StemMapper

--- src/main/java/org/csulb/edu/raghu/keyword/keywordextraction/KeyWordExtractionCleanseStemMapper.java
+++ src/main/java/org/csulb/edu/raghu/keyword/keywordextraction/KeyWordExtractionCleanseStemMapper.java
@@ -1,9 +1,45 @@
 package org.csulb.edu.raghu.keyword.keywordextraction;
 
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.csulb.edu.raghu.keyword.util.Posting;
 
-public class KeyWordExtractionCleanseStemMapper extends Mapper<LongWritable,Text,Text,Text> {
+public class KeyWordExtractionCleanseStemMapper extends
+		Mapper<LongWritable, Text, Text, Text> {
+
+	Posting posting;
+	@Override
+	protected void cleanup(Context context) throws IOException,
+			InterruptedException {
+		
+	}
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		posting = new Posting(value.toString());
+		printMap(posting.getTokens());
+		
+	}
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		
+	}
+	
+	public static void printMap(Map mp) {
+	    Iterator it = mp.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        System.out.println(pair.getKey() + " = " + pair.getValue());
+	        it.remove(); // avoids a ConcurrentModificationException
+	    }
+	}
 
 }
--- src/main/java/org/csulb/edu/raghu/keyword/keywordextraction/KeyWordExtractionDriver.java
+++ src/main/java/org/csulb/edu/raghu/keyword/keywordextraction/KeyWordExtractionDriver.java
@@ -1,15 +1,33 @@
 package org.csulb.edu.raghu.keyword.keywordextraction;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.csulb.edu.raghu.keyword.util.KeyWordExtractionConstants;
+import org.csulb.edu.regex.RegexFileInputFormat;
 
-public class KeyWordExtractionDriver extends Configured implements Tool{
+public class KeyWordExtractionDriver extends Configured implements Tool {
 
 	public int run(String[] arg0) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+
+		Configuration configuration = this.getConf();
+		Job job = new Job(configuration, KeyWordExtractionConstants.JOBNAME);
+
+		job.setMapperClass(KeyWordExtractionCleanseStemMapper.class);
+		job.setNumReduceTasks(KeyWordExtractionConstants.ZERO);
+		job.setInputFormatClass(RegexFileInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		return job.waitForCompletion(KeyWordExtractionConstants.TRUE) ? KeyWordExtractionConstants.ZERO
+				: KeyWordExtractionConstants.ONE;
+
 	}
-	
-	
+
+	public static void main(String[] args) throws Exception {
+		System.exit(ToolRunner.run(new KeyWordExtractionDriver(), args));
+	}
 
 }
